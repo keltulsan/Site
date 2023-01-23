151209794cd2e6e9d3dfb918c1221933aa006d31
Maquereauu <macrorush@gmail.com>
2023-01-23 15:32:55 +0100

    update:bdd2

--- back/index.js
+++ back/index.js
@@ -1,8 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 4444;
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('eko', 'root', 'root', {
   host: 'localhost',
-  dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
