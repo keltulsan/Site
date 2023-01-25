c98c386425bfb4a0b077ce3f18cea25bb4379747
Gwenitora <gwenitora.gardu74@gmail.com>
2023-01-25 18:18:26 +0100

    feature: catégories

--- front/myapp/src/components/header.js
+++ front/myapp/src/components/header.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const getAllLabels = async () => {
     const response = await fetch(
@@ -16,23 +16,37 @@ const getAllLabels = async () => {
 }
 
 export function Header() {
+    const [ labs, setLabs ] = useState([]);
     const [ labels, setLabels ] = useState([]);
     useEffect(() => {
         const labelsFetched = getAllLabels();
         labelsFetched
-            .then(result => setLabels(result))
-            .catch(error=>console.error("Erreur avec notre API :",error.message));
+        .then(result => setLabels(result))
+        .catch(error=>console.error('Erreur avec notre API :',error.message));
     },[]);
+    useEffect(() => {
+        if (labels.length > 0) {
+            setLabs(labels.map(val => 
+                <li key={val["label_name"]}>{val["label_name"]}</li>
+            ))
+        }
+    },[labels]);
 
-    return <div className="navbar">
-        <Link to="/"><img className="logo" src="./img/logo.png" alt="Logo de Eko"/></Link>
-        <div className="flex align-center">
-            <Link to="/categories"><p>Catégories</p></Link>
-            <Link to="/my-sellings"><p>Mes ventes</p></Link>
-            <Link to="/actus"><p>Actus</p></Link>
-            <Link to="/login"><p>Login</p></Link>
-            <Link to="/bag"><img src="./img/shopping-bag.png" alt="Logo du panier de Eko"/></Link>
-            <Link to="/research"><img src="./img/search.png" alt="Logo de recherche de Eko"/></Link>
+    return <div className='navbar'>
+        <div className='flex space-between'>
+            <Link to='/'><img className='logo' src='./img/logo.png' alt='Logo de Eko'/></Link>
+            <div className='flex align-center'>
+                <Link to='/categories'><p>Catégories</p></Link>
+                <Link to='/my-sellings'><p>Mes ventes</p></Link>
+                <Link to='/actus'><p>Actus</p></Link>
+                <Link to='/login'><p>Login</p></Link>
+                <Link to='/bag'><img src='./img/shopping-bag.png' alt='Logo du panier de Eko'/></Link>
+                <Link to='/research'><img src='./img/search.png' alt='Logo de recherche de Eko'/></Link>
+            </div>
         </div>
+        
+        <div><ul>
+            {labs}
+        </ul></div>
     </div>
 }
