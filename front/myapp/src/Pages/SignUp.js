8f11886e759ac101263dde5f2a3b1b3146b08653
Maquereauu <macrorush@gmail.com>
2023-01-26 16:13:50 +0100

    update:sur ma route oui le sign up  fini oui une vie de route

--- front/myapp/src/Pages/SignUp.js
+++ front/myapp/src/Pages/SignUp.js
@@ -1,27 +1,55 @@
-import red_square_bg from '../Img/cr2.png';
-import Button from 'react-bootstrap/Button';
-function SignUp(props){
+import icon_email from '../Img/icon_email.png';
+import icon_password from '../Img/icon_password.png';
+function Login(props){
     return <div className="Background">
         <div className="container">
         <div className="row">
-        <div className="col-md-10 offset-md-1 col-12">
-        <h1>404</h1>
-        <h2>La page sélectionnée est introuvable</h2>
+        <div className="col-md-12 col-12">
+        <div className="bg">
+
+        <form>
+        <div className="space2">
+        <h1>SIGN UP</h1>
+        </div>
+        <div className="row ">
+        <div className="col-5">
+        <img src={icon_email} alt="icon_email" className="col-1 offset-11"/>
+        </div>
+        <div className="col-7">
+        <input placeholder="Nom d'utilisateur" type="text" id="username"/>
         </div>
         </div>
-        <div className="row">
-        <div className="col-lg-4 offset-lg-3 col-md-4 offset-md-4 col-11 offset-1">
-        <div className="col-lg-9 offset-lg-5 col-md-9 offset-md-3 col-8 offset-2 Big_Button">
-            <div className="logo">
-            <img src={red_square_bg} alt="Logo"/>
-            <Button href="/" variant="secondary" className="button bg-transparent border-0" >
-            Menu
-            </Button>
+        <div className="row rowForm2">
+        <div className="col-7 offset-5">
+        <input placeholder="Adresse E-mail" type="text" id="email"/>
+        </div>
+        </div>
+        <div className="row ">
+        <div className="col-5">
+        <img src={icon_password} alt="icon_email" className="col-1 offset-11"/>
+        </div>
+        <div className="col-7">
+        <input placeholder="Mot de passe" type="text" id="password"/>
+        </div>
+        </div>
+        <div className="row rowForm2">
+        <div className="col-7 offset-5">
+        <input placeholder="Confirmer mot de passe" type="text" id="email"/>
+        </div>
+        </div>
+        <div className="row rowForm2">
+            <div className='col-2 offset-4'>
+                <a href="/login" className="createAccount">Se connecter</a>
             </div>
         </div>
+        <div className="row formBottom">
+        <input className="col-2 offset-5 " type="submit" value="Créer un compte"/>
+        </div>
+        </form>
         </div>
         </div>
         </div>
-    </div>
+        </div>
+        </div>
 }
-export default SignUp;
+export default Login;
