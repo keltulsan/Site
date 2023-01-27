d1a5391768b77d78de1404a2905619e99a6fa68c
ewen <ebeaufils@gaming.tech>
2023-01-27 10:36:34 +0100

    update : contacDetails

--- front/myapp/src/pages/ContactDetails.js
+++ front/myapp/src/pages/ContactDetails.js
@@ -1,59 +1,47 @@
 export function ContactDetails(props) {
-    return <div className="Background">
-        <div className="container">
-            <div className="row centerObject">
-                <div className="center">
-                    <h1 className="title top">Mes coordonnées</h1>
+    return <form>
+        <h1 className="title top">Mes coordonnées</h1>
+        <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop2 margin0'>Coordonnées</h2>
+        <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
+            <input placeholder="Adresse E-mail" type="text" id="email" />
+            <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop margin0'>Adresse de livraison</h2>
+            <input placeholder="Pays" type="text" id="pays" />
+
+            <div>
+                <div>
+                    <input placeholder="Prénom" type="text" id="prenom" />
                 </div>
-                <form>
-                    <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop2 margin0'>Coordonnées</h2>
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-                        <input placeholder="Adresse E-mail" type="text" id="email" />
-                    </div>
-                    <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop margin0'>Adresse de livraison</h2>
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-                        <input placeholder="Pays" type="text" id="pays" />
-                    </div>
+                <div>
+                    <input className='offset-md-1' placeholder="Nom" type="text" id="nom" />
+                </div>
+            </div>
 
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-                        <div className="row col-md-6 col-sm-12  ">
-                            <input placeholder="Prénom" type="text" id="prenom" />
-                        </div>
-                        <div className="row col-md-6 col-sm-12 margin0 ">
-                            <input className='offset-md-1' placeholder="Nom" type="text" id="nom" />
-                        </div>
-                    </div>
+            <input placeholder="Entreprise (falcultatif)" type="text" id="business" />
 
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-                        <input placeholder="Entreprise (falcultatif)" type="text" id="business" />
-                    </div>
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0 ">
-                        <input placeholder="Adresse" type="text" id="adress" />
-                    </div>
-                    <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-                        <input placeholder="Appartement (falcultatif)" type="text" id="apartment" />
-                    </div>
+            <div>
+                <input placeholder="Adresse" type="text" id="adress" />
 
-                    <div className="row col-md-6 col-sm-12 marginTop margin0">
-                        <div className="row col-md-6 col-sm-12  ">
-                            <input placeholder="Code postal" type="text" id="code-postal" />
-                        </div>
-                        <div className="row col-md-6 col-sm-12 margin0">
-                            <input className='offset-md-1' placeholder="Ville" type="text" id="city" />
-                        </div>
-                    </div>
 
-                    <div className="row col-md-6 offset-md-3  marginTop margin0">
-                        <input placeholder="Téléphone" type="text" id="phone-number" />
+                <input placeholder="Appartement (falcultatif)" type="text" id="apartment" />
+
+
+                <div>
+                    <div>
+                        <input placeholder="Code postal" type="text" id="code-postal" />
                     </div>
-                    <div className='row center'>
-                        <p className='text littleSize'>Sauvegarder mes coordonnées pour la prochaine fois</p>
+                    <div>
+                        <input className='offset-md-1' placeholder="Ville" type="text" id="city" />
                     </div>
-                    <div className='row col-md-12 offset-md-1'>
-                        <button className="bouton col-md-4 margin0">Poursuivre vers l'achat</button>
-                    </div>
-                </form>
+                </div>
+
+                <input placeholder="Téléphone" type="text" id="phone-number" />
+            </div>
+            <div className='row center'>
+                <p className='text littleSize'>Sauvegarder mes coordonnées pour la prochaine fois</p>
+            </div>
+            <div className='row col-md-12 offset-md-1'>
+                <button className="bouton col-md-4 margin0">Poursuivre vers l'achat</button>
             </div>
         </div>
-    </div>
+    </form>
 }
