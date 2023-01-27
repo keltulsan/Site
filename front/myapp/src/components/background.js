1af3317bacd6f87af13efb4459e9d1764348ea4c
Gwenitora <gwenitora.gardu74@gmail.com>
2023-01-27 17:44:35 +0100

    feature: background generated

--- front/myapp/src/components/background.js
+++ front/myapp/src/components/background.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Background(props) {
     const [imgsMap, setImgsMap] = useState([]);
@@ -20,22 +20,55 @@ export default function Background(props) {
     })
 
     useEffect(() => {
-        if (imgs.length < Math.floor(dimensions.width * dimensions.height / 100000)) {
-            var newImg = {
-                dir: "./img/background/" + (Math.floor(Math.random() * 48)).toString() + ".png",
-                rot: Math.floor(Math.random() * 360),
-                size: Math.floor(Math.random() * 490 + 10),
-                pos_x: Math.floor(Math.random() * (dimensions.width + 300) - 150),
-                pos_y: Math.floor(Math.random() * (dimensions.height + 300) - 150)
+        if (imgs.length < Math.floor(dimensions.width * dimensions.height / 50000)) {
+            var newImgs = imgs
+            for (let i = imgs.length; i < Math.floor(dimensions.width * dimensions.height / 50000); i++) {
+                var newImg = {
+                    dir: "./img/background/" + (Math.floor(Math.random() * 47)).toString() + ".png",
+                    rot: (Math.random() * 360).toString(),
+                    speed_rot: (Math.random() * 0.1).toString(),
+                    direct: (Math.random() * 360).toString(),
+                    speed_direct: (Math.random() * 0.1).toString(),
+                    size: (Math.random() * 125 + 50).toString(),
+                    pos_x: (Math.random() * (dimensions.height + 200) - 100).toString(),
+                    pos_y: (Math.random() * (dimensions.width + 200) - 100).toString()
+                }
+                newImgs[newImgs.length] = newImg
             }
-            setImgs([imgs, newImg])
-        }
+            console.log("new", newImgs)
+            setImgs(newImgs)
+        } else {
 
-        setImgsMap(imgs.map(val => 
-            <img src={val.dir} alt="Image du background d'Eko"/>
-        ))
+            console.log("imgs", imgs)
+            if (imgs.length > 0) {
+                setImgsMap(
+                    imgs.map((val, i) =>
+                        <img key={i} src={val.dir} 
+                            style={{
+                                position: 'absolute',
+                                zIndex: -999,
+                                rotation: val.rot + 'deg',
+                                width: val.size + 'px',
+                                top: val.pos_x + 'px',
+                                left: val.pos_y + 'px'
+                            }}
+                            alt="Une des images du background d'Eko"
+                        />
+                    )
+                )
+            }
+        }
     }, [imgs]);
-    useEffect(() => {
-        setImgs([])
-    }, []);
+
+    return <div 
+        style={{
+            zIndex: -999,
+            position: 'fixed',
+            overflow: 'hidden',
+            width: '100vw',
+            height: '100vh',
+            top: '0px'
+        }}>
+        {imgsMap}
+    </div>
 }
