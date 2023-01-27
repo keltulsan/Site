f5cbf0596162af4bced2b5b6714795b6ade65ef4
Gwenitora <gwenitora.gardu74@gmail.com>
2023-01-27 11:32:11 +0100

    update: style

--- front/myapp/src/pages/ContactDetails.js
+++ front/myapp/src/pages/ContactDetails.js
@@ -1,47 +1,33 @@
-export function ContactDetails(props) {
-    return <form>
-        <h1 className="title top">Mes coordonnées</h1>
-        <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop2 margin0'>Coordonnées</h2>
-        <div className="row col-md-6 offset-md-3 col-sm-12 marginTop margin0">
-            <input placeholder="Adresse E-mail" type="text" id="email" />
-            <h2 className='subTitle row col-6 offset-md-3 col-sm-12 marginTop margin0'>Adresse de livraison</h2>
-            <input placeholder="Pays" type="text" id="pays" />
+export function ContactDetails() {
+    return <div className="flex vertical"><form className="align-center">
+        <h1 className="title">Mes coordonnées</h1>
+        <div className="flex vertical gap center">
+            <h2 className='title top left'>Coordonnées</h2>
+            <input className='background my-account' placeholder="Adresse E-mail" type="text" id="email" />
 
+            <h2 className='title left'>Adresse de livraison</h2>
+            <input className='background my-account' placeholder="Pays" type="text" id="pays" />
+            <div className="flex gap-vw">
+                <input className='background my-account-' placeholder="Prénom" type="text" id="prenom" />
+                <input className='background my-account-' placeholder="Nom" type="text" id="nom" />
+            </div>
+            <input className='background my-account' placeholder="Entreprise (falcultatif)" type="text" id="business" />
+            <input className='background my-account' placeholder="Adresse" type="text" id="adress" />
+            <input className='background my-account' placeholder="Appartement (falcultatif)" type="text" id="apartment" />
+            <div className="flex gap-vw">
+                <input className='background my-account-' placeholder="Code postal" type="text" id="code-postal" />
+                <input className='background my-account-' placeholder="Ville" type="text" id="city" />
+            </div>
+            <input className='background my-account' placeholder="Téléphone" type="text" id="phone-number" />
+            
+            {/* after */}
+            <div className='align-top flex gap-'>
+                <input type="checkbox" className="align-center" />
+                <p className="text align-center">Sauvegarder mes coordonnées pour la prochaine fois</p>
+            </div>
             <div>
-                <div>
-                    <input placeholder="Prénom" type="text" id="prenom" />
-                </div>
-                <div>
-                    <input className='offset-md-1' placeholder="Nom" type="text" id="nom" />
-                </div>
-            </div>
-
-            <input placeholder="Entreprise (falcultatif)" type="text" id="business" />
-
-            <div>
-                <input placeholder="Adresse" type="text" id="adress" />
-
-
-                <input placeholder="Appartement (falcultatif)" type="text" id="apartment" />
-
-
-                <div>
-                    <div>
-                        <input placeholder="Code postal" type="text" id="code-postal" />
-                    </div>
-                    <div>
-                        <input className='offset-md-1' placeholder="Ville" type="text" id="city" />
-                    </div>
-                </div>
-
-                <input placeholder="Téléphone" type="text" id="phone-number" />
-            </div>
-            <div className='row center'>
-                <p className='text littleSize'>Sauvegarder mes coordonnées pour la prochaine fois</p>
-            </div>
-            <div className='row col-md-12 offset-md-1'>
-                <button className="bouton col-md-4 margin0">Poursuivre vers l'achat</button>
+                <input type="submit" value="Poursuivre vers l'achat" />
             </div>
         </div>
-    </form>
+    </form></div>
 }
