87d95a7860dde86bb07dc8fd52f95722def85f7b
Gwenitora <gwenitora.gardu74@gmail.com>
2023-01-27 14:58:35 +0100

    update: rand system

--- front/myapp/src/components/background.js
+++ front/myapp/src/components/background.js
@@ -1,3 +1,36 @@
-export default function Background(props){
+import { useEffect, useState } from "react";
 
+export default function Background(props) {
+    const [imgs, setImgs] = useState([]);
+
+    const [dimensions, setDimensions] = React.useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    })
+    React.useEffect(() => {
+        function handleResize() {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth
+            })
+        }
+
+        window.addEventListener('resize', handleResize)
+    })
+
+    useEffect(() => {
+        if (imgs.length < Math.floor(dimensions.width * dimensions.height / 100000)) {
+            var newImg = {
+                dir: "./img/background/" + (Math.floor(Math.random() * 48)).toString() + ".png",
+                rot: Math.floor(Math.random() * 360),
+                size: Math.floor(Math.random() * 490 + 10),
+                pos_x: Math.floor(Math.random() * (dimensions.width + 300) - 150),
+                pos_y: Math.floor(Math.random() * (dimensions.height + 300) - 150)
+            }
+            setImgs([imgs, newImg])
+        }
+    }, [imgs]);
+    useEffect(() => {
+        setImgs([])
+    }, []);
 }
