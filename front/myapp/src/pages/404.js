c82a0c33933e07be02b94901656065b4b8f61fa5
Gwenitora <gwenitora.gardu74@gmail.com>
2023-01-27 01:19:19 +0100

    debug: 404

--- front/myapp/src/pages/404.js
+++ front/myapp/src/pages/404.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 export default function Error404(props){
     return <>
-        <h1 className='title top'>404</h1>
+        <h1 className='title top'>error 404</h1>
         <h2 className='title top'>La page sélectionnée est introuvable</h2>
         <div className="err-qcq flex center">
             <Link to="/" className="align-center">Menu</Link>
