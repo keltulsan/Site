1cc4124151255e02fb66f377fb6c8a89daef8264
ewen <ewen.beaufils09@gmail.com>
2023-01-28 15:26:07 +0100

    feature : history page

--- front/myapp/src/pages/History.js
+++ front/myapp/src/pages/History.js
@@ -1,50 +1,35 @@
 export function History() {
     return <div className='container'>
         <h1 className='title'>Votre historique sur Eko</h1>
-        <div className="grid2 gap-plus center">
-            <div className="grid2 vertical gap">
-            <div className="grid2 gap">
-                <div className="grid2 box">
-                    <img classname="icon" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+        <div className="gap-plus center">
+            <div className="vertical gap">
+                <div className="gap grid2 margin-top flex">
+                    <div className="box default-size">
+                        <img className="full-width" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                    </div>
+                    <div className="box flex align-center">
+                        <p className="text">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                    </div>
                 </div>
-                <div className="grid2 box">
-                    <p className="text">What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-</p>
+                <div className="gap grid2 margin-top flex">
+                    <div className="box default-size">
+                        <img className="full-width" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                    </div>
+                    <div className="box flex align-center">
+                        <p className="text">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                    </div>
                 </div>
-            </div>
-            <div className="grid2 gap">
-                <div className="grid2 gap box">
-                    <img classname="icon" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                <div className="gap grid2 margin-top flex">
+                    <div className="box default-size">
+                        <img className="full-width" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                    </div>
+                    <div className="box flex align-center">
+                        <p className="text">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                    </div>
                 </div>
-                <div className="grid2 box">
-                    <p className="text">What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-</p>
-                </div>
-            </div>
-            <div className="grid2 gap">
-                <div className="grid2 gap box">
-                    <img classname="icon" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
-                </div>
-                <div className="grid2 box">
-                    <p className="text">What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-</p>
-                </div>
-            </div>
             </div>
         </div>
     </div>
