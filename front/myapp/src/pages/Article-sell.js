49eb2a38475c512a55c4adc60ad81420f0d7ae6c
ewen <ewen.beaufils09@gmail.com>
2023-01-29 15:12:02 +0100

    feature : Page vente vetement

--- front/myapp/src/pages/Article-sell.js
+++ front/myapp/src/pages/Article-sell.js
@@ -1,3 +1,37 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+function SliderVetements(){
+        return (
+            <Carousel>
+                <div className='left'>
+                    <img className="icon-carousel" src="./img/fichier 4.png" alt="Image d'article sur Eko" />
+                </div>
+                <div>
+                    <img className="icon-carousel" src="./img/dot single 1.png" alt="Image d'article sur Eko"/>
+                </div>
+                <div>
+                    <img className="icon-carousel" src="./img/fichier 3.png" alt="Image d'article sur Eko"/>
+                </div>
+                <div>
+                    <img className="icon-carousel" src="./img/fichier 5.png" alt="Image d'article sur Eko"/>
+                </div>
+                <div>
+                    <img className="icon-carousel" src="./img/fichier 6.png" alt="Image d'article sur Eko"/>
+                </div>
+            </Carousel>
+        );
+};
+
+
+
+
+
+
+
+
 export function ArticleSell() {
     return <div className='container'>
         <h1 className='title stroke'>Mes articles sur Eko</h1>
@@ -5,17 +39,12 @@ export function ArticleSell() {
             <div className="gap-plus center">
                 <div className="flex vertical gap margin-top">
 
-                    <h2 className='title left stroke'>Votre carte bancaire</h2>
-                    <input className='background my-account' placeholder="Numéro de carte" type="text" id="card-numbers" />
-                    <div className="flex gap-vw">
-                        <input className='background my-account-' placeholder="Date d'éxpiration " type="text" id="expiration-date" />
-                        <input className='background my-account-' placeholder="Cryptogramme" type="text" id="cryptogram" />
-                    </div>
-                    <input className='background my-account' placeholder="Titulaire" type="text" id="incumbent" />
+                    <SliderVetements />
+
 
                     <div className='align-top flex gap-'>
                         <input type="checkbox" className="align-center" defaultChecked={true} />
-                        <p className="text align-center stroke">Sauvegarder mes coordonnées pour la prochaine fois</p>
+                        <p className="text align-center stroke">En stock</p>
                     </div>
 
 
@@ -25,23 +54,44 @@ export function ArticleSell() {
                 <div className="background-color-1-5 padding-top- radius-top">
                 </div>
                 <div className="margin-top- flex vertical">
-                    <h2 className="title left">Prix <img className="icon" src="./img/euro.png" alt="icone euro sur eko"></img></h2>
-                    <h2 className="title left margin-top--- ">Date de livraison <img className="icon" src="./img/calendrier.png" alt="icone de calendrier sur eko"></img></h2>
-                    <h2 className="title left margin-top--- ">Expéditeur <img className="icon" src="./img/groupe.png" alt="icone de groupe sur eko"></img></h2>
+                    <h2 className="title left">Prix <img className="icon" src="./img/euro.png" alt="icone euro sur Eko"></img></h2>
+                    <h2 className="title left margin-top--- ">Date de livraison <img className="icon" src="./img/calendrier.png" alt="icone de calendrier sur Eko"></img></h2>
+                    <h2 className="title left margin-top--- ">Expéditeur <img className="icon" src="./img/groupe.png" alt="icone de groupe sur Eko"></img></h2>
                     <div className="flex vertical margin-top--- ">
                         <h2 className="title left">Note de l'article </h2>
                         <div className="left margin-top--- ">
-                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur eko"></img>
-                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur eko"></img>
-                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur eko"></img>
-                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur eko"></img>
-                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur eko"></img>
+                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur Eko"></img>
+                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur Eko"></img>
+                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur Eko"></img>
+                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur Eko"></img>
+                            <img className="icon" src="./img/etoile.png" alt="icone d'étoile sur Eko"></img>
                         </div>
-                    <h2 className="title margin-top--- left">Récapitulatif du panier</h2>
+                        <div className="margin-top--">
+                            <h2 className="title left">Quantité </h2>
+                            <select className="left background-color-2-3 drop-down radius">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                            </select>
+                        </div>
                     </div>
-                    <h2 className="title margin-top--">'nom produit+quantité'</h2>
+                    <div className="margin-top--">
+                        <h2 className="title left">Taille <img className="icon" src="./img/size-guide.png" alt="icone d'un guide des tailles sur Eko"></img></h2>
+                        <select className="left background-color-2-3 drop-down radius">
+                            <option value=""></option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                        </select>
+                    </div>
                     <div className="margin-top-- flex center margin-bottom---">
-                        <input className="padding-right-left" type="submit" value="Valider mon panier" />
+                        <div className="flex center">
+                            <input className="padding-right-left" type="submit" value="Ajouter au panier"></input>
+                            <img className="icon" src="./img/ajout-panier.png" alt="icone d'ajouter au panier sur Eko"></img>
+                        </div>
                     </div>
                 </div>
             </div>
