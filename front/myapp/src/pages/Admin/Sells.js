61ddee1f7da9dede2eb21a478164154d4a2679ec
keltulsan <ewen.beaufils09@gmail.com>
2023-01-30 19:10:34 +0100

    Update Sells.js

--- front/myapp/src/pages/Admin/Sells.js
+++ front/myapp/src/pages/Admin/Sells.js
@@ -1,1 +1,62 @@
 import { Link } from "react-router-dom"
+import React, { Component, useEffect, useState } from 'react';
+
+export function Sells() {
+
+    const [dimensions, setDimensions] = React.useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+        })
+    React.useEffect(() => {
+        function handleResize() {
+            setDimensions({
+            height: window.innerHeight,
+            width: window.innerWidth
+        })}
+
+        window.addEventListener('resize', handleResize)
+    })
+
+    return <div className='container'>
+        <h1 className='title stroke'> Mes ventes sur Eko</h1>
+        <div className={"flex center "+(dimensions.width <= 750 ? " vertical margin-top- gap" : " gap-plus margin-top")}>
+            <div className="flex vertical align-center">
+                <div className="flex gap box background-color-2-4">
+                    <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                </div>
+                <div className="flex center gap-">
+                    <Link className="style-link stroke" to="/">Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to="/"> Supprimer</Link>
+                </div>
+            </div>
+            <div className="flex vertical gap- align-center">
+                <div className="flex gap box background-color-2-4">
+                    <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                </div>
+                <div className="flex center gap-">
+                    <Link className="style-link stroke" to="/">Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to="/"> Supprimer</Link>
+                </div>
+            </div>
+            <div className="flex vertical align-center">
+                <div className="flex gap box background-color-2-4">
+                    <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
+                    <img className="align-center" src='./img/paypal.png' alt='image de paiment paypal sur Eko' />
+                </div>
+                <div className="flex center gap-">
+                    <Link className="style-link stroke" to="/">Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to="/"> Supprimer</Link>
+                </div>
+
+            </div>
+        </div>
+        <div className="flex center margin-top">
+            <Link className="style-link-2" to="/"><h2 className="title border stroke background-button">Mes ventes</h2></Link>
+        </div>
+        <div className="flex center margin-top">
+            <Link className="style-link-2" to="/"><img className="align-center icon-plus" src='./img/bouton play.png' alt='Bouton lancement de vidéo explicative sur les ventes sur Eko' /></Link>
+        </div>
+    </div>
+}
