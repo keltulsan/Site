4ba63e3d4d2ea25ca2bf9092226463986ae75a9d
ewen <ebeaufils@gaming.tech>
2023-01-31 08:17:28 +0100

    Update : Trie app.js

--- front/myapp/src/App.js
+++ front/myapp/src/App.js
@@ -22,7 +22,7 @@ import { ContactUs } from './pages/Us/Contact-Us';
 import { ThxEmail } from './pages/Us/Thx-Email';
 import Actus from './pages/Us/Actus';
 import PayChoice from './pages/Payment/PaymentChoice';
-import {Stocks } from './pages/Admin/Stocks';
+import { Stocks } from './pages/Admin/Stocks';
 import { Sells } from './pages/Admin/Sells';
 import { EnvyList } from './pages/Article/Envy-list';
 import { History } from './pages/Inform-user/History';
@@ -32,67 +32,78 @@ import { Bag } from './pages/Inform-user/Bag';
 import { Articles } from './pages/Article/Articles';
 import { UserPage } from './pages/Inform-user/UserPage';
 import Background from './components/background';
-import { ArticleClotheSell} from './pages/Article/Article-clothe-sell';
+import { ArticleClotheSell } from './pages/Article/Article-clothe-sell';
 import { ArticleSell } from './pages/Article/Article-sell';
 import { FAQ } from './pages/Us/Faq';
 
 
-export default function App(props){
-  return <>{/*<Background />*/}<div className="root flex space-between vertical">
+export default function App(props) {
+  return <>{/*<Background />*/}
     <Router>
       <Header />
-      <Switch>
-
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/actus" component={Actus} />
+      <div className="root flex space-between vertical">
         
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-        
-        <Route exact path="/account-user" component={UserPage} />
-        <Route exact path="/account" component={ContactDetails} />
-        <Route exact path="/eko-save" component={EkoSave} />
+        <Switch>
+
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
+
+          <Route exact path="/eko-save" component={EkoSave} />
+
+          <Route exact path="/chat-bot" component={ChatBox} />
+          
+
+          {/* Inform user */}
+
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/account-user" component={UserPage} />
+          <Route exact path="/account" component={ContactDetails} />
+          <Route exact path="/panier" component={Bag} />
+          <Route exact path="/history" component={History} />
+
+          {/* Article */}
+
+          <Route exact path="/articles" component={Articles} />
+          <Route exact path="/article-clothe-sell" component={ArticleClotheSell} />
+          <Route exact path="/article-sell" component={ArticleSell} />
+          <Route exact path="/envy-list" component={EnvyList} />
+
+          {/* Payment */}
+
+          <Route exact path="/payed-complete" component={PayComplete} />
+          <Route exact path="/pay-choice" component={PayChoice} />
+
+          {/* Admin */}
+
+          <Route exact path="/stocks" component={Stocks} />
+          <Route exact path="/sells" component={Sells} />
 
 
-        <Route exact path="/payed-complete" component={PayComplete} />
+          {/* Condition générale */}
 
-        <Route exact path="/panier" component={Bag} />
+          <Route exact path="/cgu" component={CGU} />
+          <Route exact path="/cgv" component={CGV} />
+          <Route exact path="/cgr" component={CGR} />
+          <Route exact path="/ml" component={ML} />
 
-        <Route exact path="/articles" component={Articles} />
+          {/* Us */}
 
-        <Route exact path="/article-clothe-sell" component={ArticleClotheSell} />
-        <Route exact path="/article-sell" component={ArticleSell} />
-        
+          <Route exact path="/about-us" component={AboutUs} />
+          <Route exact path="/thx-email" component={ThxEmail} />
+          <Route exact path="/contact" component={ContactUs} />
+          <Route exact path="/faq" component={FAQ} />
+          <Route exact path="/actus" component={Actus} />
 
-        <Route exact path="/thx-email" component={ThxEmail} />
+          {/* Error */}
 
-        <Route exact path="/faq" component={FAQ} />
+          <Route exact path="*" component={Error404} />
 
-        <Route exact path="/stocks" component={Stocks} />
-        <Route exact path="/sells" component={Sells} />
-        <Route exact path="/envy-list" component={EnvyList} />
-
-        <Route exact path="/pay-choice" component={PayChoice} />
-
-        <Route exact path="/history" component={History} />
-
-        <Route exact path="/chat-bot" component={ChatBox} />
-
-        <Route exact path="/contact" component={ContactUs} />
-        
-        <Route exact path="/cgu" component={CGU} />
-        <Route exact path="/cgv" component={CGV} />
-        <Route exact path="/cgr" component={CGR} />
-        <Route exact path="/ml" component={ML} />
-        <Route exact path="/about-us" component={AboutUs} />
-
-        <Route exact path="*" component={Error404} />
-        
-      </Switch>
-      <Robot />
+        </Switch>
+        <Robot />
+      </div>
       <Footer />
     </Router>
-    </div></>
+  </>
 }
 
