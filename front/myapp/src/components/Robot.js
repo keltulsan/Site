bda71d41a7285b5a67342780c36624ee9888679c
ewen <ebeaufils@gaming.tech>
2023-01-31 13:02:03 +0100

    update : robot

--- front/myapp/src/components/Robot.js
+++ front/myapp/src/components/Robot.js
@@ -4,8 +4,8 @@ import { links } from "../App";
 
 export function Robot() {
     const link = links();
-    return<div className="container flex max-height fixed right z-index-">
-    <div className="flex align-bot">
+    return<div className=" fixed container flex max-height right-f z-index- sticky">
+    <div className="align-bot ">
         <Link to={link.homeFull}><img className="robot" src="./img/bot.png" alt="Image du chatbot sur Eko"/></Link>
     </div>
     </div>
