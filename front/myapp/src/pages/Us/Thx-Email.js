a72129e409216186d7194cb733a6976f0d976999
ewen <ebeaufils@gaming.tech>
2023-02-01 10:28:44 +0100

    debug : orthographe

--- front/myapp/src/pages/Us/Thx-Email.js
+++ front/myapp/src/pages/Us/Thx-Email.js
@@ -7,7 +7,7 @@ export function ThxEmail(props) {
     return <div className="flex vertical gap-plus">
         <h1 className="title top stroke">Merci</h1>
         <div className="flex center">
-            <p className='text align-center stroke'>Nous vous recontacteront par mail dans les plus bref delais.</p>
+            <p className='text align-center stroke'>Nous vous recontacterons par mail dans les plus bref delais.</p>
         </div>
         <div className='text align-center'>
             <Link to={link.home}><input type="submit" value="Retourner au menu" /></Link>
