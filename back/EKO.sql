77c0e790b255e8c87c0cc40215c4f7e2c366e25c
ewen <ebeaufils@gaming.tech>
2023-02-01 11:43:13 +0100

    update : BDD

--- back/EKO.sql
+++ back/EKO.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 31 Janvier 2023 à 09:16
+-- Généré le :  Mer 01 Février 2023 à 10:42
 -- Version du serveur :  5.7.11
 -- Version de PHP :  7.0.3
 
@@ -34,6 +34,22 @@ CREATE TABLE `action` (
   `link_name` varchar(100) NOT NULL,
   `img` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `action`
+--
+
+INSERT INTO `action` (`id`, `name`, `link_name`, `img`) VALUES
+(1, 'Achat', 'achat', NULL),
+(2, 'Vente', 'vente', NULL),
+(3, 'Recherche', 'recherche', NULL),
+(4, 'Modification du profil', 'modification-du-profil', NULL),
+(5, 'Modification de l\'entreprise', 'modification-de-l\'entreprise', NULL),
+(6, 'Nouveau vendeur', 'nouveau-vendeur', NULL),
+(7, 'Remove vendeur', 'remove-vendeur', NULL),
+(8, 'Ban', 'ban', NULL),
+(9, 'Abonnement Eko-save', 'abonnement-eko-save', NULL),
+(10, 'Amélioration du badge', 'amelioration-du-badge', NULL);
 
 -- --------------------------------------------------------
 
@@ -70,6 +86,13 @@ CREATE TABLE `business` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `business`
+--
+
+INSERT INTO `business` (`id`, `name`, `img`, `activity`, `creation_date`, `business_email`, `business_phone`, `business_password`, `business_adress`, `business_name`, `business_banking_informations`, `user_id`) VALUES
+(1, 'Eko', 'icons-user.jpg', 'E-commerce', '2023-02-01 09:44:12', 'contacteko.pro@gmail.com', '0676967586', 'GwendalChefProjetEko1#', '11 Rue de Marseille Lyon', 'Eko', '', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -80,8 +103,18 @@ CREATE TABLE `condition` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `link_name` varchar(100) NOT NULL,
-  `desc` int(11) DEFAULT NULL
+  `desc` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `condition`
+--
+
+INSERT INTO `condition` (`id`, `name`, `link_name`, `desc`) VALUES
+(1, 'utilisation', 'utilisation', 'En acceptant les conditions générales d\'utilisation, l\'utilisateur accepte que : eko récupère et conserve les ses coordonnées bancaire et leurs utilisations. Il accepte aussi que eko utilise ses données comme bon luii semble. L\'utilisateur accepte que eko puisse voler ses biens matériels grâce à leur adresse'),
+(2, 'vente', 'vente', 'En acceptant les conditions générales de ventes, l\'utilisateur accepte de ne pas être remboursé dans le cas où sont achat n\'est jamais arrivé à destination. Il accepte de donner l\'entièreté de ses gains fait sur eko en faisant un don à l\'entreprise eko.'),
+(3, 'rétractation', 'retractation', 'En acceptant les générales de rétractations l\'utilisateur accepte de ne pas pouvoir se rétracter lors des achats.'),
+(4, 'Mention l\'égale', 'mention-l\'egale', 'En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site https:/eko.com/ l’identité des différents intervenant dans le cadre de sa réalisation et de son suivi. L\'utilisateur accepte de fournir tout ses biens moraux et matériels et virtuels à Eko.');
 
 -- --------------------------------------------------------
 
@@ -105,6 +138,15 @@ CREATE TABLE `faq` (
   `name` varchar(100) NOT NULL,
   `desc` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `faq`
+--
+
+INSERT INTO `faq` (`id`, `name`, `desc`) VALUES
+(1, 'Qu\'est ce que Eko ?', 'Eko est un site de vente de biens eko-responable.'),
+(2, 'Depuis quand Eko existe-t\'il ?', 'Eko existe depuis le 6 août 2021'),
+(3, 'Quel est l\'obejectif de Eko ?', 'Nous cherchons à révolutionner l’e-commerce en prenant\r\ncompte des problématiques sociétales actuelles à savoir la\r\npollution de la planète et la défavorisation du commerce local.');
 
 -- --------------------------------------------------------
 
@@ -143,6 +185,29 @@ INSERT INTO `labels` (`id`, `label_name`, `link_name`, `img`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `link_name` varchar(100) NOT NULL,
+  `desc` varchar(1000) NOT NULL,
+  `img` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `news`
+--
+
+INSERT INTO `news` (`id`, `name`, `link_name`, `desc`, `img`) VALUES
+(1, 'Nouveau nombre d\'utilisateur', 'nouveau-nombre-d\'utilisateur ', 'Merci à vous, grâce à vous nous venons de comptabilisé nombre premier visiteur sur notre site.', NULL),
+(2, 'Arnaque', 'arnaque', 'Nous n\'arnaquons pas nos utilisateurs, bienvenue sur Eko', NULL),
+(3, 'On a choisi l\'e-commerce', 'on-a-choisi-l\'e-commerce', 'Pourquoi, feur', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `parainnage`
 --
 
@@ -152,6 +217,13 @@ CREATE TABLE `parainnage` (
   `parainated_id` int(11) NOT NULL,
   `link_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `parainnage`
+--
+
+INSERT INTO `parainnage` (`id`, `parain_id`, `parainated_id`, `link_name`) VALUES
+(1, 1, 2, 'ewen-Gwendal');
 
 -- --------------------------------------------------------
 
@@ -218,13 +290,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `img`, `nickname`, `mail`, `phone_number`, `adress`, `apartment`, `code_postal`, `city`, `password`, `banking_informations`, `acceptance_of_the_gtcu`, `stay_connect`, `newsletter`, `eko_experience`, `badges`, `admin`, `seller`, `eko_save`, `envy_id`) VALUES
-(1, NULL, 'icons-user.jpg', 'b', 'a', NULL, NULL, NULL, NULL, NULL, 'cc', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, NULL),
-(2, NULL, 'icons-user.jpg', 'b', 'a', NULL, NULL, NULL, NULL, NULL, 'e0323a9039add2978bf5b49550572c7c', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, NULL),
-(3, NULL, 'icons-user.jpg', 'bb', 'aa', NULL, NULL, NULL, NULL, NULL, 'e0323a9039add2978bf5b49550572c7c', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, NULL),
-(4, NULL, 'icons-user.jpg', 'it\'s a me', 'monmail', NULL, NULL, NULL, NULL, NULL, 'mdp', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, 1),
-(5, NULL, 'icons-user.jpg', 'ewen', 'mail', NULL, NULL, NULL, NULL, NULL, '8f8ad28dd6debff410e630ae13436709', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, 0),
-(6, NULL, 'icons-user.jpg', 'ewen1', 'mail', NULL, NULL, NULL, NULL, NULL, '8f8ad28dd6debff410e630ae13436709', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, 0),
-(7, NULL, 'icons-user.jpg', 'admin', 'admin', NULL, NULL, NULL, NULL, NULL, '21232f297a57a5a743894a0e4a801fc3', NULL, 0, 0, 0, NULL, NULL, 0, 0, 0, 0);
+(1, 'ewen', 'icons-user.jpg', 'Keltulsan', 'ebeaufils@gaming.tech', '0676967586', '11 rue d\'Hanoï', '102', '69100', 'Lyon', 'ewen', NULL, 1, 1, 1, NULL, NULL, 1, 1, 1, NULL),
+(2, 'Gwendal', 'icons-user.jpg', 'Gwenitora', 'gacquart-reylans@gaming.tech', NULL, NULL, NULL, NULL, NULL, '', NULL, 0, 0, 0, NULL, NULL, 1, 1, 1, 0);
 
 --
 -- Index pour les tables exportées
@@ -276,6 +343,12 @@ ALTER TABLE `labels`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `parainnage`
 --
 ALTER TABLE `parainnage`
@@ -312,7 +385,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `action`
 --
 ALTER TABLE `action`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `analytics`
 --
@@ -322,27 +395,32 @@ ALTER TABLE `analytics`
 -- AUTO_INCREMENT pour la table `business`
 --
 ALTER TABLE `business`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `condition`
 --
 ALTER TABLE `condition`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `faq`
 --
 ALTER TABLE `faq`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `labels`
 --
 ALTER TABLE `labels`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
+-- AUTO_INCREMENT pour la table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT pour la table `parainnage`
 --
 ALTER TABLE `parainnage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `product`
 --
@@ -352,7 +430,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
