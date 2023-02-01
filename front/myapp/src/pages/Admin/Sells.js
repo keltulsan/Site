613517a1359c9b3021ff3ea996243c53aa483588
ewen <ebeaufils@gaming.tech>
2023-02-01 10:34:49 +0100

    update : style

--- front/myapp/src/pages/Admin/Sells.js
+++ front/myapp/src/pages/Admin/Sells.js
@@ -22,7 +22,7 @@ export function Sells() {
     return <div className='container'>
         <h1 className='title stroke'> Mes ventes sur Eko</h1>
         <div className={"flex center "+(dimensions.width <= 750 ? " vertical margin-top- gap" : " gap-plus margin-top")}>
-            <div className="flex vertical align-center">
+            <div className="flex vertical gap- align-center">
                 <div className="flex gap box background-color-2-4">
                     <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
                     <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
@@ -42,7 +42,7 @@ export function Sells() {
                     <Link className="style-link stroke" to={link.homeFull}>Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to={link.homeFull}> Supprimer</Link>
                 </div>
             </div>
-            <div className="flex vertical align-center">
+            <div className="flex vertical gap- align-center">
                 <div className="flex gap box background-color-2-4">
                     <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
                     <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
@@ -55,7 +55,7 @@ export function Sells() {
             </div>
         </div>
         <div className="flex center margin-top">
-            <Link className="style-link-2" to={link.homeFull}><h2 className="title border stroke background-button">Mes ventes</h2></Link>
+            <Link className="style-link-2" to={link.stocks}><h2 className="title border stroke background-button">Mes stocks</h2></Link>
         </div>
         <div className="flex center margin-top">
             <Link className="style-link-2" to={link.homeFull}><img className="align-center icon-plus" src='./img/bouton play.png' alt='Bouton lancement de vidéo explicative sur les ventes sur Eko' /></Link>
--- front/myapp/src/pages/Admin/Stocks.js
+++ front/myapp/src/pages/Admin/Stocks.js
@@ -23,7 +23,7 @@ export function Stocks() {
     return <div className='container'>
         <h1 className='title stroke'> Mes stocks sur Eko</h1>
         <div className={"flex center "+(dimensions.width <= 750 ? " vertical margin-top- gap" : " gap-plus margin-top")}>
-            <div className="flex vertical">
+            <div className="flex vertical gap-">
                 <div className="flex gap box background-color-2-4 align-center">
                     <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
                     <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
@@ -33,7 +33,7 @@ export function Stocks() {
                     <Link className="style-link stroke" to={link.homeFull}>Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to={link.homeFull}> Supprimer</Link>
                 </div>
             </div>
-            <div className="flex vertical align-center">
+            <div className="flex vertical gap- align-center">
                 <div className="flex gap box background-color-2-4">
                     <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
                     <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
@@ -43,7 +43,7 @@ export function Stocks() {
                     <Link className="style-link stroke" to={link.homeFull}>Modifier </Link><p className="text"> / </p><Link className="style-link stroke" to={link.homeFull}> Supprimer</Link>
                 </div>
             </div>
-            <div className="flex vertical align-center">
+            <div className="flex vertical gap- align-center">
                 <div className="flex gap box background-color-2-4">
                     <img className="align-center" src='./img/mastercard.png' alt='image de paiment mastercard sur Eko' />
                     <img className="align-center" src='./img/visa.png' alt='image de paiment mastercard sur Eko' />
@@ -56,7 +56,7 @@ export function Stocks() {
             </div>
         </div>
         <div className="flex center margin-top">
-            <Link className="style-link-2" to={link.homeFull}><h2 className="title border stroke background-button">Mes stocks</h2></Link>
+            <Link className="style-link-2" to={link.sells}><h2 className="title border stroke background-button">Mes ventes</h2></Link>
         </div>
         <div className="flex center margin-top">
         <Link className="style-link-2" to={link.homeFull}><img className="align-center icon-plus" src='./img/bouton play.png' alt='Bouton lancement de vidéo explicative sur les stocks sur Eko' /></Link>
