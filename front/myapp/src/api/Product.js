048fbf3bc49d93473ae34be939c24e2d6d4f232b
Maquereauu <macrorush@gmail.com>
2023-02-02 15:52:03 +0100

    update

--- front/myapp/src/api/Product.js
+++ front/myapp/src/api/Product.js
@@ -1,4 +1,4 @@
-export const Product_list = async () => {
+export const ProductList = async () => {
     const response = await fetch(
         'http://localhost:4444/product/list', {
             method: 'GET', 
@@ -10,4 +10,19 @@ export const Product_list = async () => {
     )
     const ProductList = await response.json()
     return ProductList
+}
+
+export const ProductUpdate = async (user) => {
+    const response = await fetch(
+        'http://localhost:4444/product/update', {
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json', 
+                'Content-Type':'application/json'
+            },
+            body : JSON.stringify(user)
+        }
+    )
+    const ProductUpdate = await response.json()
+    return ProductUpdate
 }
