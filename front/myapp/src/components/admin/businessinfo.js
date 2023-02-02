6b5fd69773e8f487bc12114535864fc2d6cd87b3
ewen <ewen>
2023-02-02 18:27:07 +0100

    update : style

--- front/myapp/src/components/admin/businessinfo.js
+++ front/myapp/src/components/admin/businessinfo.js
@@ -8,7 +8,8 @@ export function EnterprisePage(props) {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmitUpdateUser = async (data) => {
         UpdateBusiness_(data)
-        window.location.replace('/');}
+        window.location.replace('/');
+    }
     const [dimensions, setDimensions] = React.useState({
         height: window.innerHeight,
         width: window.innerWidth
@@ -21,31 +22,31 @@ export function EnterprisePage(props) {
             })
         }
         window.addEventListener('resize', handleResize)
-    });     
-    return props.info?<div className="flex vertical">
+    });
+    return props.info ? <div className="flex vertical">
         <form onSubmit={handleSubmit(onSubmitUpdateUser)} className="align-center flex vertical center" >
             <h1 className="title flex center margin-top--">Mon Entreprise</h1>
             <div className="flex vertical gap center">
                 <div className='flex2 center vertical'>
-                <div className="profils-account align-center ">
-                    <img className={"img-modal "+(dimensions.width <= 750 ? " img-modal- " : "")} src='./img/avatar.png' alt='ton avatar sur Eko' />
-                </div>
-                <h2 className='title top left align-center'>Information</h2>
-                <div className="flex gap-vw align-center">
-                    <input type="hidden" {...register("id")} value={props.info.id}/>
-                    <input className='background my-account- margin-top---' {...register("name")} placeholder="Nom" type="text" id="Nom-prenom" defaultValue={props.info.name} />
-                    <input className='background my-account- margin-top---' {...register("activity")} placeholder="Activité" type="text" id="nickname" defaultValue={props.info.activity} />
-                </div>
-                <div className="flex gap-vw align-center">
+                    <div className="profils-account align-center ">
+                        <img className={"img-modal " + (dimensions.width <= 750 ? " img-modal- " : "")} src='./img/avatar.png' alt='ton avatar sur Eko' />
+                    </div>
+                    <h2 className='title top left align-center'>Information</h2>
+                    <div className="flex gap-vw align-center">
+                        <input type="hidden" {...register("id")} value={props.info.id} />
+                        <input className='background my-account- margin-top--- margin-right--' {...register("name")} placeholder="Nom" type="text" id="Nom-prenom" defaultValue={props.info.name} />
+                        <input className='background my-account- margin-top---' {...register("activity")} placeholder="Activité" type="text" id="nickname" defaultValue={props.info.activity} />
+                    </div>
+                    <div className="flex align-center">
+                        <input className='background my-account align-center margin-top--- margin-right--' {...register("business_adress")} placeholder="Adresse" type="text" id="adress" defaultValue={props.info.business_adress} />
+                        <input className='background my-account align-center margin-top---' {...register("business_phone")} placeholder="Téléphone" type="text" id="phone-number" defaultValue={props.info.business_phone} />
+                    </div>
+                    
                     <input className='background my-account margin-top---' {...register("business_email")} placeholder="Adresse E-mail" type="text" id="email" defaultValue={props.info.business_email} />
-                </div>
-                <input className='background my-account align-center margin-top---' {...register("business_adress")} placeholder="Adresse" type="text" id="adress" defaultValue={props.info.business_adress} />
-                <input className='background my-account align-center margin-top---' {...register("business_phone")} placeholder="Téléphone" type="text" id="phone-number" defaultValue={props.info.business_phone} />
-
-                <div className='flex2 center margin-top--'>
-                    <input type="submit" value="Enregistrer mes changements" />
-                </div>
+                    <div className='flex2 center margin-top--'>
+                        <input type="submit" value="Enregistrer mes changements" />
+                    </div>
                 </div>
             </div>
-        </form></div>:<div><p>oops</p></div>
+        </form></div> : <div><p>oops</p></div>
 }
