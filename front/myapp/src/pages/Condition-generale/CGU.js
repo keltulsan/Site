104cd2567f460488e21d73078e75594e0fb43ab2
William <williamgabali@gmail.com>
2023-02-03 01:25:50 +0100

    Non connard

--- front/myapp/src/pages/Condition-generale/CGU.js
+++ front/myapp/src/pages/Condition-generale/CGU.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
-const getAllCGU = async () => {
+const getAllCgu = async () => {
     const response = await fetch(
-        'http://localhost:4444/condition/utilisation', {
+        'http://localhost:4444/condition/list', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -9,12 +9,33 @@ const getAllCGU = async () => {
             }
         }
     )
+    const cgu = await response.json()
+    return cgu
 }
 
 
-export function CGU(props) {
+export function CGU() {
+    const [cgu, setCgu] = useState([]);
+    const [res, setRes] = useState([]);
+
+
+    useEffect(() => {
+        const cguFetched = getAllCgu();
+        cguFetched
+            .then(result => setCgu(result))
+            .catch(error => console.error('Erreur avec notre API :', error.message));
+    }, []);
+
+    useEffect(() => {
+        try{
+            console.log(cgu)
+            setRes(<>{cgu[0].desc}</>)
+        }catch(err){}
+    },[cgu])
+
+
     return <div className="container">
         <h1 className="title top stroke">Condition générale d'utilisation</h1>
-        <p className='text stroke'></p>
+        <p className='text stroke'>{res}</p>
     </div>
 }
