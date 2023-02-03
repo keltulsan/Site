3cd6d4205b2ebc188a29a0ff66e2cacf2d5d1d42
William <williamgabali@gmail.com>
2023-02-03 03:46:12 +0100

    dsl Faq pas finie trop fatigué pour continuer

--- front/myapp/src/pages/Us/Faq.js
+++ front/myapp/src/pages/Us/Faq.js
@@ -1,7 +1,23 @@
 import { Collapse } from 'react-collapse';
 import React, { Component, useEffect, useState } from 'react';
+const getAllFaq = async () => {
+    const response = await fetch(
+        'http://localhost:4444/faq/list', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    const faq = await response.json()
+    return faq
+}
 
 export function FAQ() {
+    const [faq, setFaq] = useState([]);
+    const [res, setRes] = useState([]);
+
     const [isClick, setIsClick] = useState(0);
     const [faqs, setFaqs] = useState(null);
 
@@ -19,6 +35,21 @@ export function FAQ() {
         window.addEventListener('resize', handleResize)
     })
 
+    useEffect(() => {
+        const faqFetched = getAllFaq();
+        faqFetched
+            .then(result => setFaq(result))
+            .catch(error => console.error('Erreur avec notre API :', error.message));
+    }, []);
+
+    useEffect(() => {
+        try{
+            console.log(faq)
+            setRes(<>{faq[0].desc}</>)
+            setRes(<>{faq[1].desc}</>)
+            setRes(<>{faq[2].desc}</>)
+        }catch(err){}
+},[faq])
 
     useEffect(() => {
         setFaqs(() => {
@@ -42,14 +73,12 @@ export function FAQ() {
                 {faqs}
                 <div className="gap margin-top flex">
                     <div className="box background-color-2-4 flex align-center">
-                        <p className="text">What is Lorem Ipsum?
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <p className="text">{res}</p>
                     </div>
                 </div>
                 <div className="gap margin-top flex">
                     <div className="box background-color-2-4 flex align-center">
-                        <p className="text">What is Lorem Ipsum?
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <p className="text">{res}</p>
                     </div>
                 </div>
             </div>
